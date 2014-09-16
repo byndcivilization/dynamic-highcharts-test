6eafe0e0a18777631e0c42ea69d749602fab58e0
Chris Perry <cperry1848@gmail.com>
2014-09-16 13:17:54 -0400

    fixed api pull, fixed date formatting

--- js/main.js
+++ js/main.js
@@ -5,14 +5,9 @@ $(document).ready(function () {
 	localhost.dateArray = [];
 	localhost.chart = {xAxisMin : null, xAxisMax : null}
 	localhost.data = {num_contributors : [], ip : [], fpu : [], civpol : [], eom : [], troops : [], all : []}
-	// localhost.type = '';
-	// localhost.values = [];
 
 
-	// var url = 'http://localhost:8080/ppp_api/aggregates';
-	// var url = 'http://ec2-54-208-218-182.compute-1.amazonaws.com:8080/ppp_api/aggregates';
-	var url = 'test.json';
-	// var url = 'http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json&vew=basic';
+	var url = 'http://localhost:8080/ppp_api/aggregates';
 	var eom = url + '/eom';
 	var police = url + '/police';
 	var troops = url + '/troops';
@@ -22,7 +17,12 @@ $(document).ready(function () {
 	//gets data from dataIn object array and pushes to dataOut array
 	function getData(dataIn, dataOut) {
 		for (var i = 0; i < dataIn.length; i++) {
-			dataOut.push(dataIn[i].value);
+			var year = dataIn[i]['date'].slice(0, 4);
+			var month = dataIn[i]['date'].slice(5, 7);
+			var day = dataIn[i]['date'].slice(8, 10);
+			var d = Date.UTC(year, month, day);
+			var value = [d, dataIn[i].value];
+			dataOut.push(value);
 		};
 		return dataOut;
 	}
@@ -30,27 +30,18 @@ $(document).ready(function () {
 	$.ajax({
 		url: url,
 		cache: false,
-		// dataType: 'jsonp',
-		dataType: 'json',
+		// dataType: 'json',
+		dataType: 'jsonp',
 		context: localhost,
 		success: function(outputfromserver) {
 
-			// get and format date array
-			for (var i = 0; i < outputfromserver[0]['values'].length; i++) {
-				var year = outputfromserver[0]['values'][i]['date'].slice(0, 4);
-				var month = outputfromserver[0]['values'][i]['date'].slice(5, 7);
-				var day = outputfromserver[0]['values'][i]['date'].slice(8, 10);
-				var d = new Date(year,month,day);
-				this.dateArray .push(d);
-			};
+			getData(outputfromserver[0].values, this.data.num_contributors);
 
 			maxDate = new Date(Math.max.apply(null,this.dateArray));
 			minDate = new Date(Math.min.apply(null,this.dateArray));
 			localhost.chart.xAxisMax = maxDate.getFullYear();
 			localhost.chart.xAxisMin = minDate.getFullYear();
-			localhost.xAxisCategories = [localhost.minYear,localhost.maxYear]
 
-			
 			// parse in data from api
 			for (var i = 0; i < outputfromserver.length; i++) {
 				switch(outputfromserver[i].cont_type) {
@@ -79,39 +70,10 @@ $(document).ready(function () {
 						console.log('key not found');
 				}
 			};
-			// data format for each category - array of arrays with data folowed by value
-			// data: [
-   //              [Date.UTC(1970,  9, 27), 0   ],
-   //              [Date.UTC(1970, 10, 10), 0.6 ],
-   //              [Date.UTC(1970, 10, 18), 0.7 ],
-   //              [Date.UTC(1970, 11,  2), 0.8 ],
-   //              [Date.UTC(1970, 11,  9), 0.6 ],
-   //              [Date.UTC(1970, 11, 16), 0.6 ],
-   //              [Date.UTC(1970, 11, 28), 0.67],
-   //              [Date.UTC(1971,  0,  1), 0.81],
-   //              [Date.UTC(1971,  0,  8), 0.78],
-   //              [Date.UTC(1971,  0, 12), 0.98],
-   //              [Date.UTC(1971,  0, 27), 1.84],
-   //              [Date.UTC(1971,  1, 10), 1.80],
-   //              [Date.UTC(1971,  1, 18), 1.80],
-   //              [Date.UTC(1971,  1, 24), 1.92],
-   //              [Date.UTC(1971,  2,  4), 2.49],
-   //              [Date.UTC(1971,  2, 11), 2.79],
-   //              [Date.UTC(1971,  2, 15), 2.73],
-   //              [Date.UTC(1971,  2, 25), 2.61],
-   //              [Date.UTC(1971,  3,  2), 2.76],
-   //              [Date.UTC(1971,  3,  6), 2.82],
-   //              [Date.UTC(1971,  3, 13), 2.8 ],
-   //              [Date.UTC(1971,  4,  3), 2.1 ],
-   //              [Date.UTC(1971,  4, 26), 1.1 ],
-   //              [Date.UTC(1971,  5,  9), 0.25],
-   //              [Date.UTC(1971,  5, 12), 0   ]
-   //          ]
 
 			this.chart.data.series.push({name : 'Troops', data : this.data.troops});
 			this.chart.data.series.push({name : 'Experts on mission', data : this.data.eom});
 			this.chart.data.series.push({name : 'Police', data : this.data.civpol});
-			// this.chart.data.xAxis.categories = this.dateArray;
 			chart = new Highcharts.Chart(this.chart.data);
 		}
 	});	
@@ -128,28 +90,21 @@ $(document).ready(function () {
 			text: 'Source: <a href="http://www.providingforpeacekeeping.org/contributions/">IPI Peacekeeping Database</a>'
 		},
 		// format for xaxis data
-		// xAxis: {
-  //           type: 'datetime',
-  //           dateTimeLabelFormats: { // don't display the dummy year
-  //               month: '%e. %b',
-  //               year: '%b'
-  //           },
-  //           title: {
-  //               text: 'Date'
-  //           }
-  //       },
 		xAxis: {
-			categories: null, //set to null and get from api and push to array
-			tickmarkPlacement: 'on',
-			title: {
-				enabled: false
-			}
-		},
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+                // month: '%e. %b',
+                year: '%Y'
+            },
+            title: {
+                text: 'Date'
+            }
+        },
 		yAxis: {
 			title: {
 				text: 'Personnel contributions' // change text and formatter to match
 			},
-			max: 100500,
+			// max: 100500,
 			labels: {
 				formatter: function () {
 					return this.value;
